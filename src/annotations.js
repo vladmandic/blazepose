f91e33f4e68c19c19f6fb8d771f96cc8716a4edd
Vladimir Mandic <mandic00@live.com>
2021-04-25 09:15:34 -0400

    implement detect, full, upper models

--- src/annotations.js
+++ src/annotations.js
@@ -1,71 +1,73 @@
-export const full = [
-  'nose',
-  'leftEyeInside',
-  'leftEye',
-  'leftEyeOutside',
-  'rightEyeInside',
-  'rightEye',
-  'rightEyeOutside',
-  'leftEar',
-  'rightEar',
-  'leftMouth',
-  'rightMouth',
-  'leftShoulder',
-  'rightShoulder',
-  'leftElbow',
-  'rightElbow',
-  'leftWrist',
-  'rightWrist',
-  'leftPalm',
-  'rightPalm',
-  'leftIndex',
-  'rightIndex',
-  'leftPinky',
-  'rightPinky',
-  'leftHip',
-  'rightHip',
-  'leftKnee',
-  'rightKnee',
-  'leftAnkle',
-  'rightAnkle',
-  'leftHeel',
-  'rightHeel',
-  'leftFoot',
-  'rightFoot',
-  'midHip',
-  'forehead',
-  'leftThumb',
-  'leftHand',
-  'rightThumb',
-  'rightHand',
+/* eslint-disable no-multi-spaces */
+
+const full = [
+  'nose',            //  0
+  'leftEyeInside',   //  1
+  'leftEye',         //  2
+  'leftEyeOutside',  //  3
+  'rightEyeInside',  //  4
+  'rightEye',        //  5
+  'rightEyeOutside', //  6
+  'leftEar',         //  7
+  'rightEar',        //  8
+  'leftMouth',       //  9
+  'rightMouth',      // 10
+  'leftShoulder',    // 11
+  'rightShoulder',   // 12
+  'leftElbow',       // 13
+  'rightElbow',      // 14
+  'leftWrist',       // 15
+  'rightWrist',      // 16
+  'leftPalm',        // 17
+  'rightPalm',       // 18
+  'leftIndex',       // 19
+  'rightIndex',      // 20
+  'leftPinky',       // 21
+  'rightPinky',      // 22
+  'leftHip',         // 23
+  'rightHip',        // 24
+  'leftKnee',        // 25
+  'rightKnee',       // 26
+  'leftAnkle',       // 27
+  'rightAnkle',      // 28
+  'leftHeel',        // 29
+  'rightHeel',       // 30
+  'leftFoot',        // 31
+  'rightFoot',       // 32
+  'midHip',          // 33
+  'forehead',        // 34
+  'leftThumb',       // 35
+  'leftHand',        // 36
+  'rightThumb',      // 37
+  'rightHand',       // 38
 ];
 
-export const upper = [
-  'nose',
-  'leftEyeInside',
-  'leftEye',
-  'leftEyeOutside',
-  'rightEyeInside',
-  'rightEye',
-  'rightEyeOutside',
-  'leftEar',
-  'rightEar',
-  'leftMouth',
-  'rightMouth',
-  'leftShoulder',
-  'rightShoulder',
-  'leftElbow',
-  'rightElbow',
-  'left:15',
-  'right:16',
-  'left:17',
-  'right:18',
+const upper = [
+  'nose',            //  0
+  'leftEyeInside',   //  1
+  'leftEye',         //  2
+  'leftEyeOutside',  //  3
+  'rightEyeInside',  //  4
+  'rightEye',        //  5
+  'rightEyeOutside', //  6
+  'leftEar',         //  7
+  'rightEar',        //  8
+  'leftMouth',       //  9
+  'rightMouth',      // 10
+  'leftShoulder',    // 11
+  'rightShoulder',   // 12
+  'leftElbow',       // 13
+  'rightElbow',      // 14
+  'leftWrist',       // 15
+  'rightWrist',      // 16
+  'leftPalm',        // 17
+  'rightPalm',       // 18
   'left:19',
   'right:20',
   'left:21',
   'right:22',
-  'leftChest',
-  'rightChest',
+  'left:23',
+  'right:24',
   'neck',
   'forehead',
   'left:27',
@@ -73,3 +75,6 @@ export const upper = [
   'left:29',
   'right:30',
 ];
+
+exports.full = full;
+exports.upper = upper;
